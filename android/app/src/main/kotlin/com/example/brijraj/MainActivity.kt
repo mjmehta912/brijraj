b411a754b6d736fa34755fa9068bbe5ddbb8c817
mjmehta912 <monilmehta912@gmail.com>
2024-12-17 17:52:43 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/brijraj/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.brijraj
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/constants/color_constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const Color kColorPrimary = Color(0xFF0098FF);
+const Color kColorBackground = Color(0xFFECF7FF);
